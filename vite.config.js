86105342347658cd01cbd02ecf03204fe32ec1d5
Federico-Gz <federicograziani96@gmail.com>
2024-05-31 10:53:44 +0200

    edit vite congif file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env.SOME_KEY': JSON.stringify(env.SOME_KEY)
+    },
+    plugins: [
+      react(),
+      viteStaticCopy({
+        targets: [
+          {
+            src: 'public/*',
+            dest: 'public'
+          }
+        ]
+      })
+    ],
+  }
 })
